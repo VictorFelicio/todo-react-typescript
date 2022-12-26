121f64a9e310f0e8f036a5ed7eb49199026d4371
Victor Felicio <victor_felicio@hotmail.com>
2022-12-26 01:58:50 -0300

    create a component to render tasks

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//components
+import TableTasks from './components/TableTasks'
+
+const App = () => {
+    return (
+        <>
+            <TableTasks />
+        </>
+    )
+}
+
+export default App
